495e5c40a0d6b82b8da60180b7e0c71aae33ad39
farukulWD <farukgb1999@gmail.com>
2024-11-02 23:14:47 +0600

    user register success

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express, { Application } from "express"
 import cors from "cors"
+import router from "./app/routes/routes"
+import cookieParser from "cookie-parser"
 
 const app:Application = express()
+
 app.use(cors())
+
+app.use(cookieParser())
 app.use(express.json())
 
-
+app.use('/api/v1', router);
 
 
 export default app
