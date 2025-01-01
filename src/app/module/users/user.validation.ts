93b057d02dd2041bf012210c6310670ba70d8360
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2025-01-01 22:49:32 +0600

    user create

--- src/app/module/users/user.validation.ts
+++ src/app/module/users/user.validation.ts
@@ -8,39 +8,38 @@ const UserStatusEnum = z.enum(['active', 'inactive']);
 const userValidationSchema = z.object({
   body: z.object({
     // userData: z.object({
-      name: z.string().min(1, { message: 'Name is required' }).optional(),
+    name: z.string().min(1, { message: 'Name is required' }).optional(),
 
-      address: z
-        .object({
-          address: z.string().optional(),
-          city: z.string().optional(),
-          thana: z.string().optional(),
-          postal: z.number().optional(),
-          country: z.enum(['Bangladesh']).default('Bangladesh'),
-        })
-        .optional(),
+    address: z
+      .object({
+        address: z.string().optional(),
+        city: z.string().optional(),
+        thana: z.string().optional(),
+        postal: z.string().optional(),
+        country: z.enum(['Bangladesh']).default('Bangladesh'),
+      })
+      .optional(),
 
+    mobile: z.string().min(10, { message: 'Mobile number is required' }),
 
-      mobile: z.string().min(10, { message: 'Mobile number is required' }),
+    // email: z.string().email({ message: 'Invalid email address' }).optional(),
 
-      // email: z.string().email({ message: 'Invalid email address' }).optional(),
+    password: z
+      .string()
+      .min(6, { message: 'Password must be at least 6 characters long' })
+      .optional(),
 
-      password: z
-        .string()
-        .min(6, { message: 'Password must be at least 6 characters long' })
-        .optional(),
+    profileImg: z.string().optional(),
 
-      profileImg: z.string().optional(),
+    passwordChangedAt: z.date().optional(),
 
-      passwordChangedAt: z.date().optional(),
+    role: UserRoleEnum.default('user').optional(),
 
-      role: UserRoleEnum.default('user').optional(),
+    status: UserStatusEnum.default('active').optional(),
 
-      status: UserStatusEnum.default('active').optional(),
-
-      isDeleted: z.boolean().default(false).optional(),
-    }),
-  })
+    isDeleted: z.boolean().default(false).optional(),
+  }),
+});
 // });
 
 // For use in request validation middleware
